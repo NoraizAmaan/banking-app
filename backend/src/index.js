e22d5ca141252081b6f2edce2869f42d48eed3b6
Noraiz Amaan <noraizamaan150303@gmail.com>
2025-09-09 10:32:33 +0530

    Secure User module with password hashing, JWT auth, input validation, and error handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,30 +3,15 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@as-integrations/express5";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cors from "cors";
-
+//import cors from "cors";
+import { typeDefs,resolvers } from "./graphql/schema.js";
+import {authMiddleware} from './middleware/auth.js'
 //Load env variables
 dotenv.config();
+           
 
 const app = express();
-
-//MongoDB connection;
-mongoose.connect(process.env.MONGO_URL)
-    .then(()=>console.log("MongoDB connected"))
-    .catch(err => console.error("MongoDB error:", err));
-
-const typeDefs = `
-  type Query {
-    hello: String
-  }
-`;
-
-
-const resolvers ={
-    Query:{
-        hello: ()=> "Hello Banking App Backend"
-    },
-};
+app.use(express.json())
 
 const server = new ApolloServer({
     typeDefs,
@@ -37,19 +22,31 @@ const startServer = async () =>{
     //Start the Apollo Server
     await server.start();
 
-    //Use expressMiddleware to integrate Apollo Server 
-    app.use(
-        '/graphql', //This is the path for your GrphQL
-        cors(),
-        express.json(),
-        expressMiddleware(server)
-    );
+//use expressMiddleware to integrate apollo server with express 
+app.use(
+    '/graphql',
+    expressMiddleware(server,{
+        context: async ({req, res})=>{
+            authMiddleware(req, res, ()=>{ });
+            // console.log(`${JSON.stringify(req.body)}`);
+            
+            return {user: req.user};
+        },
+    })
+);
+//MongoDB connection and server start;
+mongoose.connect(process.env.MONGO_URL)
+    .then(()=> {
+        console.log("MongoDB connected");
+        const PORT = process.env.PORT || 4000;
+        app.listen(PORT, ()=>{
+            console.log(`Server ready at http://localhost:${PORT}/graphql`);
+        })
+    })
+    .catch(err => console.error("MongoDB error:", err));
 
-    //Start Express Server\
-    const PORT = process.env.PORT || 4000;
-    app.listen(PORT, ()=>{
-        console.log(`Server ready at http://localhost:${PORT}/graphql`);
-    });
+
 };
 
 startServer();
+
